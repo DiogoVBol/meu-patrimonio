74341407f1e47348f22fe56353a341ab4c6fe912
diogovierib <diogovierib@gmail.com>
2026-09-24 23:52:50 -0300

    feat: configure Google OAuth Client ID

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   apiUrl: "https://script.google.com/macros/s/AKfycbzjN6d-DpAlJPWxX_PIxlSFKRFQ9iZTcwSzMLUxWj0Olm7Tc0y12Fetxt22cLl_FvZlWQ/exec",
-  googleClientId: ""
+  googleClientId: "103923735106-koom2uao2afl5o2mli2l8t5m54t0dt70.apps.googleusercontent.com"
 };
