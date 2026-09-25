046f21db84ee8a0e762a785e1028afaa9690f31b
diogovierib <diogovierib@gmail.com>
2026-09-24 23:45:41 -0300

    chore: configure Apps Script apiUrl

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  apiUrl: "",
+  apiUrl: "https://script.google.com/macros/s/AKfycbzjN6d-DpAlJPWxX_PIxlSFKRFQ9iZTcwSzMLUxWj0Olm7Tc0y12Fetxt22cLl_FvZlWQ/exec",
   googleClientId: ""
 };
